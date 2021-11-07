35b6a4a3907f82b9a3ed45faa414241c5eb3c09c
luismezaf <dmc.luisalfredo@gmail.com>
2021-11-07 15:57:19 -0300

    Pequeña mejora del footer

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -9,15 +9,34 @@ import {
     useColorScheme,
     View,
     TextInput,
-    Button
+    Button,
+    TouchableOpacity
 } from 'react-native';
 
-const Footer = ({onPageChange}) => {
-    return(<View style={styles.footer}>
-        <Button style={styles.footerButton} onPress={()=>onPageChange('pacientes')} title="Pacientes" color="orange" />
-        <Button style={styles.footerButton} onPress={()=>onPageChange('reservas')} title="Reservas" color="orange" />
-        <Button style={styles.footerButton} onPress={()=>onPageChange('fichas')} title="Fichas" color="orange" />
-    </View>);
+const FooterOption = ({onPress, title, selected}) => {
+    return (
+        <View style={[styles.footerButton, {paddingHorizontal:4}]}>
+            <TouchableOpacity onPress={onPress} style={{ backgroundColor:selected?'orange':'white', borderRadius:20 }} >
+                <Text style={{color:selected?'white':'grey', textAlign:'center', padding:10, fontSize:16}}>{title}</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const Footer = ({ onPageChange }) => {
+    const [selected, setSelected] = useState('pacientes')
+    const handleChangePage = page => {
+        onPageChange(page)
+        setSelected(page)
+    }
+
+    return (
+        <View style={styles.footer}>
+            <FooterOption selected={selected==='pacientes'} onPress={() => handleChangePage('pacientes')} title="Pacientes" />
+            <FooterOption selected={selected==='reservas'} onPress={() => handleChangePage('reservas')} title="Reservas" />
+            <FooterOption selected={selected==='fichas'} onPress={() => handleChangePage('fichas')} title="Fichas" />
+        </View>
+    );
 }
 
 export default ( {children, onPageChange} ) => {
