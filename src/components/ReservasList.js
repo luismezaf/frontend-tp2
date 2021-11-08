dd19d3c5782da39f20254a47fe158244621c04d6
David <david.nunez@oktana.io>
2021-11-08 13:41:54 -0300

    crear y borrar Reservas listo

---
+++ src/components/ReservasList.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from 'react';
+import BigList from "react-native-big-list";
+import { endpoint } from '../config/api';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    TextInput,
+    Button,
+    FlatList
+} from 'react-native';
+
+export default ({reservasList, setError, getReservas, idCliente}) => {
+    const reservar = async ({fechaCadena, horaInicioCadena, horaFinCadena, idEmpleado}, idCliente = 3) => {
+        const body = JSON.stringify({
+            fechaCadena,
+            horaInicioCadena,
+            horaFinCadena,
+            idEmpleado:{
+                idPersona: idEmpleado.idPersona
+            },
+            idCliente:{
+                idPersona:idCliente
+            }
+        });
+        const completeEndpoint = `${endpoint}/stock-nutrinatalia/reserva`;
+        try {
+            const data = await fetch(completeEndpoint, { method: 'POST', body, headers:{
+                usuario: "usuario1",
+                'Content-Type': 'application/json'
+            }});   
+            getReservas();   
+        } catch (error) {
+            console.log(error);
+            setError(error);
+        }
+    }
+    const cancelarReserva = async(idReserva) => {
+        const completeEndpoint = `${endpoint}/stock-nutrinatalia/reserva/${idReserva}`;
+        try {
+            await fetch(completeEndpoint, {method:'DELETE'});
+            getReservas();   
+        } catch (error) {
+            console.log(error);
+            setError(error);
+        }
+
+    }
+    const modificarReserva = async() => {
+        
+    }
+    const renderItem = ({item}) => ( 
+        <View style={styles.reservasListItem}>
+            <View >
+                <Text> {item.fecha.substring(0, 10)} desde {item.horaInicioCadena} hasta {item.horaFinCadena}</Text>
+                { item.idReserva && <Text>{item.idCliente.nombreCompleto}</Text>}
+            </View>
+            { item.idReserva ? 
+            <View style={styles.actions}>
+                <Button 
+                    style={styles.botonReservar}
+                    onPress={() => cancelarReserva(item.idReserva)}
+                    title="Cancelar"
+                    color="red"/> 
+                <Button 
+                    style={styles.botonModificar}
+                    onPress={() => modificarReserva(item.idReserva)}
+                    title="Modificar"
+                color="grey"/> 
+            </View>: 
+            <Button 
+                style={styles.botonReservar}
+                onPress={() => reservar(item, idCliente)}
+                title="Reservar"
+                color="orange"/>
+            }
+        </View>)
+    return(
+        <View style={styles.reservas}>
+            <BigList data={reservasList} renderItem={renderItem} renderEmpty={()=> <Text>No hay reservas para su busqueda :(</Text>}
+             itemHeight={90} />
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    botonReservar:{
+        
+    },
+    reservasListItem:{
+        height: "100%",
+        display:"flex",
+        flexDirection:"row",
+        alignItems:"center",
+        justifyContent:"space-between",
+        borderBottomWidth:2,
+        borderBottomColor:"grey",
+    },
+    actions: {
+        display:'flex',
+        height:'100%',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems:'stretch'
+    }
+});
--- src/pages/Reservas.js
+++ src/pages/Reservas.js
@@ -8,20 +8,24 @@ import {
     useColorScheme,
     View,
     TextInput,
-    Button
+    Button,
 } from 'react-native';
+
 import {Picker} from '@react-native-picker/picker';
 import DatePicker from 'react-native-date-picker';
 
 import {endpoint} from '../config/api';
+import ReservasList from '../components/ReservasList';
 
 export default () => {
     const [fisiosList, setFisiosList] = useState([]);
+    const [clientsList, setClientsList] = useState([]);
     const [reservasList, setReservasList] = useState([]);
-    const [reservasTable, setReservasTable] = useState([]);
     const [selectedFisio, setSelectedFisio] = useState(0);
+    const [selectedClient, setSelectedClient] = useState(0);
     const [date, setDate] = useState( new Date() );
     const [error, setError] = useState(null);
+
     useEffect(()=>{
         const getFisiosData = async () => {
             try {
@@ -33,53 +37,71 @@ export default () => {
                 setError(error);
             }
         }
+        const getClientsData = async () => {
+            try {
+                const data =  await fetch(`${endpoint}/stock-nutrinatalia/persona`);
+                const response = await data.json();
+                setClientsList(response.lista);
+            } catch (error) {
+                console.log(error);
+                setError(error);
+            }
+        }
         getFisiosData();
-    });
-    const formatDate = date =>( date.getFullYear() + '' + (date.getMonth() + 1) + '' + date.getDate() );
+        getClientsData();
+    }, []);
+    const formatDate = date =>`${date.getFullYear()}${(date.getMonth() + 1) >= 10 ? (date.getMonth() + 1) : `0${date.getMonth() + 1}`}${date.getDate() >= 10 ?date.getDate() : `0${date.getDate()}`}`;
     const getReservas = async() => {
-        return;
+        setReservasList([]);
         try {
-            const data =  await fetch(`${endpoint}/stock-nutrinatalia/persona/${selectedFisio}/agenda?fecha=${formatDate(date)}`);
-            const response = await data.lista;
-            setReservasList(response);
+            const completeEndpoint = `${endpoint}/stock-nutrinatalia/persona/${selectedFisio}/agenda?fecha=${formatDate(date)}`;
+            const response =  await fetch(completeEndpoint);
+            let data = await response.json();
+            if( selectedClient ){
+                data = data.filter( reserva => reserva.idCliente?.idPersona === selectedClient );
+            }
+            setReservasList(data);
         }catch (error) {
             console.log(error);
             setError(error);
         }
     }
-    if(error){
-        return(<Text>Hubo un error: {error}</Text>);
-    }
     return(
         <View>
-            <Text>
+            <Text style={styles.mainTitle}>
                 Reservas
             </Text>
-            <View style={styles.filtersSection}>
-                <Picker style={styles.fisioPicker} onValueChange={(itemValue, itemIndex) =>
-                    setSelectedFisio(itemValue)
-                } selectedValue={selectedFisio}>
-                    <Picker.Item label=" -- Selecciona un doctor -- " value={0} />
-                    {fisiosList.map( fisio => (
-                        <Picker.Item key={`${fisio.idPersona}${fisio.nombre}${fisio.apellido}`} label={`${fisio.nombre} ${fisio.apellido}`} value={fisio.idPersona} />  
-                    ))}
-                </Picker>
-                <DatePicker style={styles.datePicker} date={date} onDateChange={setDate}/>
-                <Button
-                    style={styles.botonGetReservas}
-                    onPress={getReservas}
-                    title="Buscar reservas"
-                    color="orange"
-                />
-            </View>
-            <View style={styles.reservas}>
-                {reservasList.length === 0 && <Text>No hay reservas para su busqueda :(</Text>}
-                {reservasList.map(reserva => (
-                    <View>
-                        Fecha { reserva.fecha} 
-                    </View>
-                ))}
-            </View>
+            {error ? <Text>Hubo un error!! {error.message} </Text> :
+            <>
+                <View style={styles.filtersSection}>
+                    <Picker style={styles.fisioPicker} onValueChange={(itemValue, itemIndex) =>
+                        setSelectedFisio(itemValue)
+                    } selectedValue={selectedFisio}>
+                        <Picker.Item label=" -- Selecciona un doctor -- " value={0} />
+                        {fisiosList.map( fisio => (
+                            <Picker.Item key={`${fisio.idPersona}${fisio.nombre}${fisio.apellido}`} label={`${fisio.nombre} ${fisio.apellido}`} value={fisio.idPersona} />  
+                        ))}
+                    </Picker>
+                    <Picker style={styles.fisioPicker} onValueChange={(itemValue, itemIndex) =>
+                        setSelectedClient(itemValue)
+                    } selectedValue={selectedClient}>
+                        <Picker.Item label=" -- Selecciona un cliente -- " value={0} />
+                        {clientsList.map( client => (
+                            <Picker.Item key={`${client.idPersona}${client.nombreCompleto}`} label={`${client.nombreCompleto}`} value={client.idPersona} />  
+                        ))}
+                    </Picker>
+                    <DatePicker style={styles.datePicker} date={date} onDateChange={setDate}
+                        fadeToColor='none' textColor='#fff' mode='date'
+                    />
+                    <Button
+                        style={styles.botonGetReservas}
+                        onPress={getReservas}
+                        title="Buscar reservas"
+                        color="orange"
+                    />
+                    <ReservasList reservasList={reservasList} setError={setError} getReservas={getReservas} idCliente={selectedClient}/>
+                </View>
+            </>}
         </View>
     );
 }
@@ -89,6 +111,8 @@ const styles = StyleSheet.create({
             marginRight:"auto"
         },
         datePicker:{
+            marginLeft:"auto",
+            marginRight:"auto",
             marginBottom:20
         },
         botonGetReservas:{
@@ -96,5 +120,10 @@ const styles = StyleSheet.create({
         },
         reservas: {
             marginTop: 20,
+        },
+        mainTitle:{
+            marginTop:20,
+            textAlign:"center",
+            fontSize:30
         }
 });
