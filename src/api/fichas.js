29f2a3e4db2a482240f6fdd901a644cdc70ebcb6
rduarte01 <rduarte01@hotmail.com>
2021-11-10 21:11:15 -0300

    se finaliza filtros

--- src/api/fichas.js
+++ src/api/fichas.js
@@ -24,3 +24,27 @@ export async function getFichasPaciente(){
     }
 }
 
+export async function getFisiosData() {
+    try {
+        const url =  await fetch(`${endpoint}/stock-nutrinatalia/persona?ejemplo=%7B%22soloUsuariosDelSistema%22%3Atrue%7D`);
+        const response = await fetch(url);
+        const result = await response.json();
+        return result;
+
+    }catch(error){
+        throw error;
+    }
+}
+
+export async function getClientsData() {
+    try {
+        const url =  await fetch(`${endpoint}/stock-nutrinatalia/persona`);
+        const response = await fetch(url);
+        const result = await response.json();
+        return result;
+
+    }catch(error){
+        throw error;
+    }
+
+}
--- src/pages/Fichas.js
+++ src/pages/Fichas.js
@@ -1,6 +1,10 @@
 import React, { useState,useEffect } from 'react';
-import { getFichas } from '../api/fichas';
+import { getFichas, getFichasPaciente } from '../api/fichas';
 import FichasList from '../components/FichasList';
+import axios from "axios";
+import {Picker} from '@react-native-picker/picker';
+import DatePicker from 'react-native-date-picker';
+import {endpoint} from '../config/api';
 import {
     SafeAreaView,
     ScrollView,
@@ -14,14 +18,102 @@ import {
 } from 'react-native';
 export default () => {
     const [fichas, setfichas] = useState([]);
-    console.log(fichas);
-    
+    const [fisiosList, setFisiosList] = useState([]);
+    const [clientsList, setClientsList] = useState([]);
+    const [reservasList, setReservasList] = useState([]);
+    const [selectedFisio, setSelectedFisio] = useState(0);
+    const [selectedClient, setSelectedClient] = useState(0);
+    const [date1, setDate1] = useState( new Date() );
+    const [date2, setDate2] = useState( new Date() );
+    const [error, setError] = useState(null);
+
     useEffect(()=>{
+        const getFisiosData = async () => {
+            try {
+                const data =  await fetch(`${endpoint}/stock-nutrinatalia/persona?ejemplo=%7B%22soloUsuariosDelSistema%22%3Atrue%7D`);
+                const response = await data.json();
+                setFisiosList(response.lista);
+            } catch (error) {
+                console.log(error);
+                setError(error);
+            }
+        }
+        const getClientsData = async () => {
+            try {
+                const data =  await fetch(`${endpoint}/stock-nutrinatalia/persona`);
+                const response = await data.json();
+                setClientsList(response.lista);
+            } catch (error) {
+                console.log(error);
+                setError(error);
+            }
+        }
+        getFisiosData();
+        getClientsData();
         (async () =>{
             await loadFichas();
         })()
     },[])
-
+    const formatDate1 = date1 =>`${date1.getFullYear()}${(date1.getMonth() + 1) >= 10 ? (date1.getMonth() + 1) : `0${date1.getMonth() + 1}`}${date1.getDate() >= 10 ?date1.getDate() : `0${date1.getDate()}`}`;
+    const formatDate2 = date2 =>`${date2.getFullYear()}${(date2.getMonth() + 1) >= 10 ? (date2.getMonth() + 1) : `0${date2.getMonth() + 1}`}${date2.getDate() >= 10 ?date2.getDate() : `0${date2.getDate()}`}`;
+    
+    const getFichasPaciente = async() => {
+        setfichas([]);
+        try {
+            const completeEndpoint = `${endpoint}/stock-nutrinatalia/fichaClinica?ejemplo=%7B%22idCliente%22%3A%7B%22idPersona%22%3A${selectedClient}%7D%7D`;
+            const response =  await fetch(completeEndpoint,
+                { method: 'GET', headers:{
+                    usuario: "gustavo",
+                    'Content-Type': 'application/json'
+                }}); 
+            let data = await response.json();
+            if( selectedClient ){
+                data = data.lista.filter( ficha => ficha.idCliente?.idPersona === selectedClient );
+            }
+            //console.log(data)
+            setfichas(data);
+        }catch (error) {
+            console.log(error);
+            setError(error);
+        }
+    }
+    const getFichasFisioterapeuta = async() => {
+        setfichas([]);
+        try {
+            const completeEndpoint = `${endpoint}/stock-nutrinatalia/fichaClinica?ejemplo=%7B%22idEmpleado%22%3A%7B%22idPersona%22%3A${selectedFisio}%7D%7D`;
+            const response =  await fetch(completeEndpoint,
+                { method: 'GET', headers:{
+                    usuario: "gustavo",
+                    'Content-Type': 'application/json'
+                }}); 
+            let data = await response.json();
+            if( selectedFisio ){
+                data = data.lista.filter( ficha => ficha.idEmpleado?.idPersona === selectedFisio );
+            }
+           // console.log(data)
+            setfichas(data);
+        }catch (error) {
+            console.log(error);
+            setError(error);
+        }
+    }
+    const getFichasEntreFechas = async() => {
+        setfichas([]);
+        try {
+            const completeEndpoint = `${endpoint}/stock-nutrinatalia/fichaClinica?ejemplo=%7B%22fechaDesdeCadena%22%3A${formatDate1(date1)}%2C%22fechaHastaCadena%22%3A${formatDate2(date2)}%7D`;
+            const response =  await fetch(completeEndpoint,
+                { method: 'GET', headers:{
+                    usuario: "gustavo",
+                    'Content-Type': 'application/json'
+                }}); 
+            let data = await response.json();
+            
+            setfichas(data.lista);
+        }catch (error) {
+            console.log(error);
+            setError(error);
+        }
+    }
     const loadFichas = async () =>{
 
         try {
@@ -44,9 +136,97 @@ export default () => {
         }
     }
 
+
     return(
         <View>
-            <FichasList fichas={fichas}/>
+            <Text style={styles.mainTitle}>
+                Fichas
+            </Text>
+            {error ? <Text>Hubo un error!! {error.message} </Text> :
+            <>
+                <View style={styles.filtersSection}>
+                    <Picker style={styles.fisioPicker} onValueChange={(itemValue, itemIndex) =>
+                        setSelectedFisio(itemValue)
+                    } selectedValue={selectedFisio}>
+                        <Picker.Item label=" -- Selecciona un doctor -- " value={0} />
+                        {fisiosList.map( fisio => (
+                            <Picker.Item key={`${fisio.idPersona}${fisio.nombre}${fisio.apellido}`} label={`${fisio.nombre} ${fisio.apellido}`} value={fisio.idPersona} />  
+                        ))}
+                    </Picker>
+                    <Picker style={styles.fisioPicker} onValueChange={(itemValue, itemIndex) =>
+                        setSelectedClient(itemValue)
+                    } selectedValue={selectedClient}>
+                        <Picker.Item label=" -- Selecciona un cliente -- " value={0} />
+                        {clientsList.map( client => (
+                            <Picker.Item key={`${client.idPersona}${client.nombreCompleto}`} label={`${client.nombreCompleto}`} value={client.idPersona} />  
+                        ))}
+                    </Picker>
+                    <View   >
+                    <Text style={styles.text}>Seleccione fecha de inicio</Text>
+                        <DatePicker style={styles.datePicker,styles.container} date={date1} onDateChange={setDate1}
+                            fadeToColor='none' textColor='#fff' mode='date'
+                        />
+                        <Text style={styles.text}>Seleccione fecha de fin</Text>                        
+                        <DatePicker style={styles.datePicker,styles.container} date={date2} onDateChange={setDate2}
+                            fadeToColor='none' textColor='#fff' mode='date'
+                        />
+                    </View>
+           
+                    <Button
+                        style={styles.botonGetReservas}
+                        onPress={getFichasPaciente}
+                        title="Fichas de paciente"
+                        color="orange"
+                    />
+                    <Button
+                        style={styles.botonGetReservas}
+                        onPress={getFichasFisioterapeuta}
+                        title="Fichas de fisioterapeuta"
+                        color="green"
+                    />
+                    <Button
+                        style={styles.botonGetReservas}
+                        onPress={getFichasEntreFechas}
+                        title="Fichas entre fechas"
+                        color="red"
+                    />
+                       <FichasList fichas={fichas}/>
+                </View>
+            </>}
         </View>
+        
+  
     );
 }
+const styles = StyleSheet.create({
+    filtersSection:{
+        marginLeft:"auto",
+        marginRight:"auto"
+    },
+    datePicker:{
+        marginLeft:"auto",
+        marginRight:"auto",
+        marginBottom:20
+    },
+    botonGetReservas:{
+        
+    },
+    reservas: {
+        marginTop: 20,
+    },
+    mainTitle:{
+        marginTop:20,
+        textAlign:"center",
+        fontSize:30
+    },
+    container: { 
+
+        height: 40,
+        padding:20,
+
+
+    },
+    text: { 
+        padding:10,
+    },
+});
